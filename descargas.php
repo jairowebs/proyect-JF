efb61b5ae7a765b3b041f43027746ac9e9366be9
jairowebs <136769364+jairowebs@users.noreply.github.com>
2024-01-22 20:31:27 -0500

    {}

--- descargas.php
+++ descargas.php
@@ -1,10 +1,12 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once 'vendor/autoload.php';
 
 $mpdf = new \Mpdf\Mpdf();
+
 $mpdf->WriteHTML('<h1>Hello world!</h1>');
-$mpdf->Output();
+$mpdf->Output("pdf","I");
+
 
 
 ?>
